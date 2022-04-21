d9d397fa9d9a78f41da2c6b243c3d86f51746c53
Mahdi Hosseini <mahdihosseinidevelop@gmail.com>
2022-04-21 09:51:32 +0330

    Addfeature : add feature remove task

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ loadAllEvents();
 // All events function
 function loadAllEvents() {
   form.addEventListener("submit", addTask);
+  taskList.addEventListener("click", removeTask);
 }
 
 // Function Add Tasks
@@ -19,16 +20,25 @@ function addTask(e) {
   e.preventDefault();
   if (taskInput.value == "") {
     alert("Please fill the Task field");
-  }else{
-      let li = document.createElement('li');
-      li.className = 'collection-item';
-      li.innerText = taskInput.value;
-      let link = document.createElement('a');
-      link.className = 'delete-item secondary-content';
-      link.innerHTML = '<i class="fa fa-remove"></i>'
-      li.appendChild(link);
-        taskList.appendChild(li)
-      console.log(li)
+  } else {
+    let li = document.createElement("li");
+    li.className = "collection-item";
+    li.innerText = taskInput.value;
+    let link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+    taskInput.value = "";
   }
+}
 
+// Function Remove Task
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
 }
